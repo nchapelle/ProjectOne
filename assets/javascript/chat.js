409e1407a079f13701bb41230bcebe8e1afe8cf1
Amjad Shaaban <theamjad@outlook.com>
2019-09-24 19:00:08 -0400

    Added Users online tracking from /.info/connected

--- assets/javascript/chat.js
+++ assets/javascript/chat.js
@@ -19,7 +19,18 @@ var nameBox = $("<input>")
       class: "form-control",
       placeholder: "type a message ... press [ENTER] to send"
     })
-    .appendTo(".chatbox");
+    .appendTo(".chatbox"),
+  usersOnline = $("<div>")
+    .attr("class", "online-users")
+    .text("Online users: ")
+    .appendTo(".chatbox"),
+  spanIcon = $("<span>")
+    .attr({
+      class: "glyphicon glyphicon-user",
+      id: "online-users"
+    })
+    .appendTo(usersOnline);
+
 var firebaseConfig = {
   apiKey: "AIzaSyCszCiGMtbfQW3xaAstweYKbib82r0WZF0",
   authDomain: "simple-chat-app-5aaa9.firebaseapp.com",
@@ -29,24 +40,24 @@ var firebaseConfig = {
   messagingSenderId: "556619862197",
   appId: "1:556619862197:web:bbf2cefddac59566122efc"
 };
-
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 
 var messageField = $("#messageInput"),
   nameField = $("#nameInput"),
-  messageList = $("#messages");
+  messageList = $("#messages"),
+  onlineList = $("#online-users");
 
 messageField.on("keypress", function(e) {
   if (e.keyCode === 13) {
     var username = nameField.val();
     var message = messageField.val();
 
-    database.ref().push({ name: username, text: message });
+    database.ref("/chat").push({ name: username, text: message });
     messageField.val("");
   }
 });
-database.ref().on("child_added", function(snapshot) {
+database.ref("/chat").on("child_added", function(snapshot) {
   var data = snapshot.val();
   var username = data.name || "anonymous";
   var message = data.text;
@@ -62,4 +73,18 @@ database.ref().on("child_added", function(snapshot) {
 
     messageList[0].scrollTop = messageList[0].scrollHeight;
   }
+});
+var listRef = database.ref("/presence");
+var userRef = listRef.push();
+
+var presenceRef = database.ref("/.info/connected");
+presenceRef.on("value", function(snap) {
+  if (snap.val()) {
+    userRef.set(true);
+    userRef.onDisconnect().remove();
+  }
+});
+
+listRef.on("value", function(snap) {
+  onlineList.text(snap.numChildren());
 });
