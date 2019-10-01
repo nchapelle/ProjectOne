6efad70cf60fb684cb305fb1371aea1cc0c6f673
Amjad Shaaban <theamjad@outlook.com>
2019-10-01 13:37:06 -0400

    little progress

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -49,15 +49,6 @@ var nameBox = $("<input>")
     })
     .appendTo(usersOnline);
 
-var firebaseConfig = {
-  apiKey: "AIzaSyCP2EsdbUYFADOxHdrn3u_fyBpN_Lh8kpY",
-  authDomain: "upbc-projectone.firebaseapp.com",
-  databaseURL: "https://upbc-projectone.firebaseio.com",
-  projectId: "upbc-projectone",
-  storageBucket: "",
-  messagingSenderId: "74907588730",
-  appId: "1:74907588730:web:38a07f740689c06ea283c3"
-};
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 
